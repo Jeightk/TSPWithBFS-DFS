d24e4614f006809970a389f70f0d68091b1a54e2
Jeightk <jakedmcintosh@gmail.com>
2025-09-03 14:26:44 -0400

    BFS complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import util
-
+import math
 
 xArray = []
 yArray = []
@@ -19,27 +19,27 @@ def setPositions():
     for x in range(len(xArray)):
         #create dictionary obj & add to position list
         if x == 0: # 1
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [1,2,3])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [1,2,3], spot = 0)
         elif x == 1: # 2
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [2])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [2], spot = 1)
         elif x == 2: # 3
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [3,4])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [3,4], spot = 2)
         elif x == 3: # 4
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [4,5,6])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [4,5,6], spot = 3)
         elif x == 4: # 5
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [6, 7])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [6, 7], spot = 4)
         elif x == 5: # 6
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [7])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [7], spot = 5)
         elif x == 6: # 7
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [8,9])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [8,9], spot = 6)
         elif x == 7: #8
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [8,9,10])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [8,9,10], spot = 7)
         elif x == 8: #9
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [10])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [10], spot = 8)
         elif x == 9: # 10 
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [10])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [10], spot = 9)
         elif x == 10: # 11 the end
-            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [])
+            pos = dict(xValue = xArray[x], yValue = yArray[x], touched = False, connectedTo = [], spot = 10)
 
         
         positions.append(pos)
@@ -47,11 +47,38 @@ def setPositions():
     print(xArray)
     print(yArray)
 
-def evalDistanceBFS():
+def evalDistanceBFS(posi):
 
-    for x in range(len(positions)):
-        pos = positions[x]
-        connectedTo = pos['connectedTo']
+    connectedToArray = posi['connectedTo']
+
+    if len(connectedToArray) == 0:
+        return 0
+
+    xValuep1 = posi['xValue']
+    yValuep1 = posi['yValue']
+    distanceVals = []
+    connectPositions = []
+    for x in range(len(connectedToArray)):
+        val = connectedToArray[x]
+
+        tempVal = positions[val]
+
+        xValuep2 = tempVal['xValue']
+        yValuep2 = tempVal['yValue']
+        dist = math.sqrt((xValuep2 - xValuep1)**2 + (yValuep2 - yValuep1)**2)
+        distanceVals.append(dist)
+        connectPositions.append(tempVal)
+
+    #print("vals: ", posi['connectedTo'])
+    index = distanceVals.index(min(distanceVals))
+    print("Min Distance: ", min(distanceVals))
+    nextPosi = connectPositions[index]
+    print(posi['spot'], " to ", nextPosi['spot'])
+
+    evalDistanceBFS(nextPosi)
+
+    #print(distanceVals)
+    #print(connectPositions)
 
     #find distance between both pos's
     #dist = math.sqrt((xValuep2 - xValuep1)**2 + (yValuep2 - yValuep1)**2)
@@ -59,4 +86,4 @@ def evalDistanceBFS():
 
 if __name__ == "__main__":
     setPositions()
-    evalDistanceBFS()
+    evalDistanceBFS(positions[0])
